ae4aefb47e5f6add05f69786fa6e6492e7fb9949
Shivam <shivamthakur2709@gmail.com>
2026-02-21 21:34:24 +0530

    final changes

--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ const attendanceRoutes = require("./routes/attendance.routes");
 const taskRoutes = require("./routes/task.routes");
 
 const app = express();
+const morgan = require("morgan");
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/attendance", attendanceRoutes);
